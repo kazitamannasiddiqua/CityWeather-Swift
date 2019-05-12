c659f532b1f5b563303735194cfb723377367a00
Jun Dang <jun.dang8@gmail.com>
2019-05-12 18:44:00 -0400

    Initial Commit

---
+++ CityWeather-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityWeather-3
+//
+//  Created by Jun Dang on 2019-05-12.
+//  Copyright © 2019 Jun Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
